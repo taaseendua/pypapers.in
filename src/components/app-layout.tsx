47154b350c24859169d12f4f1267424507fb3ec6
Taaseen Dua <duataaseen@gmail.com>
2025-09-24 11:31:55 +0000

    Add top menu

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -4,6 +4,15 @@ import React from 'react';
 import Link from 'next/link';
 import { Logo } from './logo';
 import { Button } from './ui/button';
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from '@/components/ui/menubar';
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -26,6 +35,60 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
           </nav>
         </div>
       </header>
+
+      <div className="border-b">
+        <div className="container">
+          <Menubar className="rounded-none border-none px-0">
+            <MenubarMenu>
+              <MenubarTrigger>File</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  New Window <MenubarShortcut>⌘N</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem disabled>New Incognito Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>
+                  Share <MenubarShortcut>⇧⌘S</MenubarShortcut>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>
+                  Print <MenubarShortcut>⌘P</MenubarShortcut>
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>Edit</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Cut</MenubarItem>
+                <MenubarItem>Copy</MenubarItem>
+                <MenubarItem>Paste</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>View</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>Reload</MenubarItem>
+                <MenubarItem>Force Reload</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Toggle Fullscreen</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Toggle Developer Tools</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </div>
+      </div>
+      
       <main className="flex-1">
         {children}
       </main>
