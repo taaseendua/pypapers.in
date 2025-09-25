12720df12d5748b26d2ca013d1e3dc1e2d4a49cb
Taaseen Dua <duataaseen@gmail.com>
2025-09-25 09:01:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-620305903-d3b09",
+  "appId": "1:387979495005:web:60a487a67afd7cc0386fb4",
+  "apiKey": "AIzaSyChrYEv9kMBs1sw66UQOOfiGLAffIfQ8v8",
+  "authDomain": "studio-620305903-d3b09.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "387979495005"
+};
